71b8762dec4c35f0ac51b174e7e9807dac389927
Khan <kayyykaiii2@gmail.com>
2026-08-04 16:35:04 +0200

    update logic in silver_sp.sql

--- scripts/silver/silver_sp.sql
+++ scripts/silver/silver_sp.sql
@@ -94,11 +94,11 @@ BEGIN
             prd_nm,
             ISNULL(prd_cost, 0) AS prd_cost,
             CASE UPPER(TRIM(prd_line)) 
-            WHEN 'R' THEN 'Road'
-            WHEN 'M' THEN 'Mountain' 
-            WHEN 'S' THEN 'Other Sales'
-            WHEN 'T' THEN 'Touring'
-            ELSE 'n/a'
+                WHEN 'R' THEN 'Road'
+                WHEN 'M' THEN 'Mountain' 
+                WHEN 'S' THEN 'Other Sales'
+                WHEN 'T' THEN 'Touring'
+                ELSE 'n/a'
         END AS prd_line,
             CAST(prd_start_dt AS DATE) AS prd_start_dt,
             CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
@@ -129,17 +129,24 @@ BEGIN
         CASE 
             WHEN sls_order_dt <= 0 OR LEN(sls_order_dt) != 8 THEN NULL
             ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)  
-            END AS sls_order_dt,
-            CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) AS sls_ship_dt,
-            CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) AS sls_due_dt,
+        END AS sls_order_dt,
+        CASE 
+            WHEN sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8 THEN NULL 
+            ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) 
+        END AS sls_ship_dt,
+        CASE 
+            WHEN sls_due_dt <= 0 OR LEN(sls_due_dt) != 8 THEN NULL 
+            ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) 
+        END AS sls_due_dt,
         CASE 
             WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price) 
             THEN sls_quantity * ABS(sls_price) ELSE sls_sales
         END AS sls_sales,
             sls_quantity,
-            CASE 
+        CASE 
             WHEN  sls_price <= 0 OR  sls_price IS NULL 
-            THEN sls_sales / NULLIF(sls_quantity, 0) ELSE sls_price 
+            THEN sls_sales / NULLIF(sls_quantity, 0) 
+            ELSE sls_price 
         END AS sls_price
         FROM bronze.crm_sales_details;
         SET @end_time = GETDATE();
